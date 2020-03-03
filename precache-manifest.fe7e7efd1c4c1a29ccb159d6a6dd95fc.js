d7a283069a3818965e65e00a35c93296a71235cc
Kushich <kushich@outlook.com>
2020-03-03 20:49:53 +0300

    Updates

---
+++ precache-manifest.fe7e7efd1c4c1a29ccb159d6a6dd95fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f873fb841ba2aa6d9e50a4a043c6387",
+    "url": "/lyric-search/index.html"
+  },
+  {
+    "revision": "5509b3674ca8feb91d99",
+    "url": "/lyric-search/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a10295adec3d2645adfa",
+    "url": "/lyric-search/static/js/2.1e93e67f.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/lyric-search/static/js/2.1e93e67f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5509b3674ca8feb91d99",
+    "url": "/lyric-search/static/js/main.fd16b61a.chunk.js"
+  },
+  {
+    "revision": "c1996364a9e94b97c21a",
+    "url": "/lyric-search/static/js/runtime-main.7b107274.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/lyric-search/static/media/spinner.26ef8bb4.gif"
+  }
+]);
